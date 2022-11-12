57874271b99ecbfd31d1b35b41e8ed76c58f3e63
emre <emre@Ubuntu22.04.myguest.virtualbox.org>
2022-11-12 16:54:30 +0300

    write buffers with multiple processes

--- forktest.c
+++ forktest.c
@@ -3,30 +3,78 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, int** argv)
+int global_counter = 0;
+int main(int argc, int **argv)
 {
-    fork(); // 2
-    fork(); // 4
-    printf("Hello WOrld\n");
-    // int f_id = fork();
-    // if (f_id < 0)
+
+    // fork(); // 2
+    // fork(); // 4
+    // for (int i = 0; i < 3; i++)
     // {
-    //     /* UNABLE TO CREATE CHILD */
-    //     printf("Couldn\'t create child process");
-    //     return 0;
-    // }
-    
-    // else if(f_id == 0)
-    // {
-    //     /* CHILD */
-    //     printf("Hello World from child:%d\n", f_id);
+    //     if (fork() == 0)
+    //     {
+    //         /* CHILD */
+    //         printf("[child] pid: %d [parent] pid: %d\n", getpid(), getppid());
+    //         exit(0);
+    //     }
     // }
 
-    // else
+    // for(int j = 0; j < 3; j++)
     // {
-    //     /* PARENT */
-    //      printf("Hello World from parent:%d\n", f_id);
+    //     wait(NULL);
     // }
+
+    for (int i = 0; i < 4; i++)
+    {
+        int f_id = fork();
         
+
+        if (f_id < 0)
+        {
+            /* UNABLE TO CREATE CHILD */
+            printf("Couldn\'t create child process");
+            return 0;
+        }
+
+        else if (f_id == 0)
+        {
+            /* CHILD */
+            // char *my_args[] = {
+            //     "m",
+            //     "myarg1",
+            //     "myarg2",
+            //     NULL};
+
+            // execvp("./forktest", my_args);
+            // for (int i = 0; i < argc; i++)
+            // {
+            //     printf("%s\n", argv[i]);
+            // }
+           
+            exit(0);
+        }
+
+        else
+        {
+            /* PARENT */
+            
+            printf("Counter: %d\n", global_counter++);
+        }
+    }
+
+    for (int i = 0; i<4; i++)
+    {
+      wait(NULL);
+    }
+    
+
+    // char *files[1024];
+    // int j = 0;
+    // for (int i = 3; i < argc - 1; i++)
+    // {
+    //     files[j] = argv[i];
+    //     j++;
+    // }
+
     return 0;
 }
--- psearch1.c
+++ psearch1.c
@@ -1,6 +1,6 @@
 /* FIX THIS
 
-- input13.txt keyword is in the 2th line but it prints as 3rd 
+- input13.txt keyword is in the 2th line but it prints as 3rd
 
 */
 
@@ -26,135 +26,179 @@ int main(int argc, int **argv)
         return 0;
     }
 
-    char *searchKeyword = argv[1]; /* DO NOT CHANGE: reserved for the keyword*/
-    int *filesCount = argv[2];     /* DO NOT CHANGE: reserved for the files count*/
-    char *outputFileName = malloc(1024);
+    // GET arguments
+    char *searchKeyword = argv[1];  /* DO NOT CHANGE: reserved for the keyword*/
+    int filesCount = atoi(argv[2]); /* DO NOT CHANGE: reserved for the files count*/
 
-    // create output file directory
-    strcat(outputFileName, ROOT_DIR);
-    strcat(outputFileName, argv[argc - 1]);
-    //printf("output file: %s\n", argv[argc - 1]);
-
-    for (int a = 3; a < argc - 1; a++)
+    // Store all file names in array
+    char *textFiles[1024];
+    int e = 0;
+    for (int d = 3; d < argc - 1; d++)
     {
+        textFiles[e] = argv[d];
+        e++;
+    }
 
-        FILE *inputFileStream; /* single stream for a file */
-        char *fileDir = argv[a];
+    /* DEBUGGING: Print input file names*/
+    // for (int i = 0; i < 1024; i++)
+    // {
+    //     if (i == 30)
+    //     {
+    //         printf("\n");
+    //     }
 
-        char currentFile[WORD_BUFFER];
-        if (currentFile == NULL)
+    //     printf("%s", textFiles[i]);
+
+    // }
+
+    /* CREATE PROCESSES */
+    int pids[filesCount]; /* store process ids */
+
+    int global_fileCounter = 0;
+    for (int c = 0; c < filesCount; c++)
+    {
+        // printf("Global counter: %d\n", global_fileCounter);
+
+        char currentInputFileDir[WORD_BUFFER];
+        char *fileDir = textFiles[global_fileCounter];
+
+        if (currentInputFileDir == NULL)
         {
             printf("No memory\n");
             return 0;
         }
 
         // Make a complete dir for the argv[i]
-        strcat(currentFile, ROOT_DIR);
-        strcat(currentFile, fileDir);
+        strcat(currentInputFileDir, ROOT_DIR);
+        strcat(currentInputFileDir, fileDir);
 
-        // Open file content
-        if ((inputFileStream = fopen(currentFile, "r")) == NULL)
+        printf("CURRENT FILE: %s\n", currentInputFileDir);
+
+        /* FORK */
+        pids[c] = fork();
+
+        if (pids[c] < 0)
         {
-            printf("Error opening file!\n");
-            return 0;
+            printf("Couldn\'t create child process.\n");
+            exit(-1);
         }
 
-        // Create matched lines indices array
-        int matchedLinesIndices[MAX_MATCHED_LINES];
-        for (int b = 0; b < MAX_MATCHED_LINES; b++)
+        else if (pids[c] == 0) /* CHILD PROCESS */
         {
-            matchedLinesIndices[b] = 0;
-        }
+            char *bufferOutputDir = malloc(1024);
 
-        int matchedLinesCount = 0;
-        int myChar;
-        int lineCount = 0;
-        char word[WORD_BUFFER];
-        int wordChCount = 0;
+            // create directories for buffer output files
+            strcat(bufferOutputDir, ROOT_DIR);                      /* ./ */
+            strcat(bufferOutputDir, "buffer_");                     /* ./buffer- */
+            strcat(bufferOutputDir, textFiles[global_fileCounter]); /* ./buffer_input1.txt*/
 
+            // Start reading input file
+            FILE *inputFileStream; /* single stream for a file */
 
-        while (((myChar = fgetc(inputFileStream)) != EOF))
-        {
-            if (myChar == '\n')
+            // Open file content
+            if ((inputFileStream = fopen(currentInputFileDir, "r")) == NULL)
             {
-                lineCount++;
+                printf("Error opening input file!\n");
+                return 0;
             }
-            // Before beginning a new word
-            if (myChar == ' ' || myChar == '\n' || myChar == '\0' || myChar == '\t')
-            {
 
-                if (strcmp(word, searchKeyword) == 0)
+            // Create matched lines indices array
+            int matchedLinesIndices[MAX_MATCHED_LINES];
+            for (int b = 0; b < MAX_MATCHED_LINES; b++)
+            {
+                matchedLinesIndices[b] = 0;
+            }
+
+            int matchedLinesCount = 0;
+            int myChar;
+            int lineCount = 0;
+            char word[WORD_BUFFER];
+            int wordChCount = 0;
+
+            // Read file by character to find matched line indices
+            while (((myChar = fgetc(inputFileStream)) != EOF))
+            {
+                if (myChar == '\n')
                 {
-                    matchedLinesIndices[matchedLinesCount] = lineCount + 1;
-                    matchedLinesCount++;
+                    lineCount++;
                 }
+                // Before beginning a new word
+                if (myChar == ' ' || myChar == '\n' || myChar == '\0' || myChar == '\t')
+                {
+                    // Is it the keyword?
+                    if (strcmp(word, searchKeyword) == 0)
+                    {
+                        matchedLinesIndices[matchedLinesCount] = lineCount + 1;
+                        matchedLinesCount++;
+                    }
 
-                // Reset word
-                word[0] = '\0';
-                wordChCount = 0;
+                    // Reset word
+                    word[0] = '\0';
+                    wordChCount = 0;
+                }
+                else
+                {
+                    word[wordChCount++] = (char)myChar;
+                    word[wordChCount] = '\0';
+                }
             }
-            else
+
+            char matchedLines[MAX_MATCHED_LINES][WORD_BUFFER];
+            char line[LINE_BUFFER];
+
+            // Write all lines to an array
+            rewind(inputFileStream);
+
+            int j = 1;
+            while ((fgets(line, LINE_BUFFER, inputFileStream)) != NULL)
             {
-                word[wordChCount++] = (char)myChar;
-                word[wordChCount] = '\0';
+                strcpy(matchedLines[j], line);
+                j++;
             }
-        }
 
-        char matchedLines[MAX_MATCHED_LINES][WORD_BUFFER];
-        char line[LINE_BUFFER];
+            // End of reading file
+            rewind(inputFileStream);
+            fclose(inputFileStream);
 
-        /* DEBUGGING */
-        // Print matched line indices
-        // for (int i = 0; i < MAX_MATCHED_LINES; i++)
-        // {
-        //     if (i % 30 == 0)
-        //     {
-        //         printf("\n");
-        //     }
-        //     printf("%d ", matchedLinesIndices[i]);
-        // }
+            // Write to output file
+            FILE *outputFileStream;
 
-        //printf("\n");
-
-        // Write all lines to an array
-        rewind(inputFileStream);
-        int j = 1;
-        while ((fgets(line, LINE_BUFFER, inputFileStream)) != NULL)
-        {
-            strcpy(matchedLines[j], line);
-            j++;
-        }
-
-        // End of reading file 
-        //printf("\n");
-        rewind(inputFileStream);
-        fclose(inputFileStream);
-        
-
-
-        // Write to output file
-        FILE * outputFileStream;
-
-        if ((outputFileStream = fopen(outputFileName, "a")) == NULL)
-        {
-            printf("Error opening output file!\n");
-            return 0;
-        }
-
-        int l = 0;
-        for (int k = 0; k < MAX_MATCHED_LINES; k++)
-        {   
-            
-
-            // TODO: write to a file
-            if (k == matchedLinesIndices[l])
+            if ((outputFileStream = fopen(bufferOutputDir, "a")) == NULL)
             {
-                fprintf(outputFileStream, "%s, %d: %s\n", currentFile, k, matchedLines[k]);
-                l++;
+                printf("Error opening output file!\n");
+                return 0;
             }
+
+            int l = 0;
+            for (int k = 0; k < MAX_MATCHED_LINES; k++)
+            {
+
+                // TODO: write to buffer file
+                if (k == matchedLinesIndices[l])
+                {
+                    fprintf(outputFileStream, "%s, %d: %s\n", currentInputFileDir, k, matchedLines[k]);
+                    l++;
+                }
+            }
+
+            fclose(outputFileStream);
+            rewind(inputFileStream);
+            rewind(outputFileStream);
+
+            exit(0);
         }
-        fclose(outputFileStream);
-        *currentFile = '\0';
+
+        else /* PARENT PROCESS */
+        {
+            global_fileCounter++;
+            // TODO: write to output.txt
+            *currentInputFileDir = '\0';
+        }
+    }
+
+    for (int c = 0; c < filesCount; c++)
+    {
+        wait(NULL);
     }
 
     return 0;
