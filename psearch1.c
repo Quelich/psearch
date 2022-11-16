173baa383fc304dba249626025dcfa247cf0acf1
emre <emre@Ubuntu22.04.myguest.virtualbox.org>
2022-11-16 17:58:37 +0300

    fix psearch1

--- psearch1.c
+++ psearch1.c
@@ -10,12 +10,13 @@
 #include <unistd.h>
 
 //#define BUFFER_SIZE 209715200 // 200 MB
-#define ROOT_DIR            "./"
-#define BUFFER_DIR          "buffer_"
-#define OUTPUT_BUFFER_SIZE  10240
-#define WORD_BUFFER         1024
-#define LINE_BUFFER         1024
-#define MAX_MATCHED_LINES   1024
+#define ROOT_DIR "./"
+#define BUFFER_DIR "buffer_"
+#define OUTPUT_BUFFER_SIZE 10240
+#define WORD_BUFFER 1024
+#define LINE_BUFFER 1024
+#define MAX_MATCHED_LINES 1024
+#define MAX_OUTPUT_FILES 10
 
 int main(int argc, int **argv)
 {
@@ -30,7 +31,7 @@ int main(int argc, int **argv)
     char *searchKeyword = argv[1];         /* DO NOT CHANGE: reserved for the keyword*/
     int filesCount = atoi(argv[2]);        /* DO NOT CHANGE: reserved for the files count*/
     char *outputFileName = argv[argc - 1]; /* DO NOT CHANGE: reserved for the output file*/
-    char *bufferOutputDirs[WORD_BUFFER];
+    char bufferOutputDirs[MAX_OUTPUT_FILES][WORD_BUFFER];
 
     // SAVE all file names in array
     char *inputFiles[1024];
@@ -41,56 +42,47 @@ int main(int argc, int **argv)
         e++;
     }
 
-    /* DEBUGGING: Print input file names*/
-    // for (int i = 0; i < 1024; i++)
-    // {
-    //     if (i == 30)
-    //     {
-    //         printf("\n");
-    //     }
-
-    //     printf("%s", textFiles[i]);
-
-    // }
-
     /* CREATE PROCESSES */
     int pids[filesCount]; /* store process ids */
 
     int global_fileCounter = 0;
+
+    // ADD buffer files to a list
+    for (int i = 0; i < filesCount; i++)
+    {
+        char bufferOutputDir[WORD_BUFFER];
+        sprintf(bufferOutputDir, "%s%s%s", ROOT_DIR, BUFFER_DIR, inputFiles[i]);
+        strcpy(bufferOutputDirs[i], bufferOutputDir);
+        // printf("ADDED: %s into output list\n", bufferOutputDirs[global_fileCounter]); /* DEBUG */
+    }
+
     for (int c = 0; c < filesCount; c++)
     {
 
         pids[c] = fork(); /* FORK A PROCESS */
 
-        char currentInputFileDir[WORD_BUFFER];
+        char currentInputFileDir[WORD_BUFFER] = {0x0};
         char *fileDir = inputFiles[global_fileCounter];
-        char *bufferOutputDir = malloc(WORD_BUFFER);
+        char bufferOutputDir[WORD_BUFFER] = {0x0};
 
         // Make a complete dir for the argv[i]
-        strcat(currentInputFileDir, ROOT_DIR);
-        strcat(currentInputFileDir, fileDir);
-
-        // printf("Global counter: %d\n", global_fileCounter);
-
+        sprintf(currentInputFileDir, "%s%s", ROOT_DIR, fileDir);
+        //printf("Current input file: %s\n", currentInputFileDir);
         // create directories for buffer output files
-        strcat(bufferOutputDir, ROOT_DIR);                       /* ./ */
-        strcat(bufferOutputDir, BUFFER_DIR);                     /* ./buffer_ */
-        strcat(bufferOutputDir, inputFiles[global_fileCounter]); /* ./buffer_input1.txt*/
+        sprintf(bufferOutputDir, "%s%s%s", ROOT_DIR, BUFFER_DIR, inputFiles[global_fileCounter]);
 
         /* Store output dir in an array for convenience */
-        bufferOutputDirs[global_fileCounter] = bufferOutputDir;
 
         if (pids[c] < 0)
         {
             printf("Couldn\'t create child process.\n");
             exit(-1);
         }
+
         else if (pids[c] == 0) /* CHILD PROCESS */
         {
 
             FILE *inputFileStream; /* single stream for a file */
-
-            //printf("CURRENT INPUT FILE: %s\n", currentInputFileDir);
 
             if (currentInputFileDir == NULL)
             {
@@ -149,9 +141,9 @@ int main(int argc, int **argv)
             }
 
             char matchedLines[MAX_MATCHED_LINES][WORD_BUFFER];
-            char line[LINE_BUFFER];
+            char line[LINE_BUFFER] = {0x0};
 
-            // Write all lines to an array
+            // RESET input file stream
             rewind(inputFileStream);
 
             int j = 1;
@@ -166,7 +158,7 @@ int main(int argc, int **argv)
             // Write to output file
             FILE *bufferOutputFileStream;
 
-            if ((bufferOutputFileStream = fopen(bufferOutputDir, "a")) == NULL)
+            if ((bufferOutputFileStream = fopen(bufferOutputDir, "w")) == NULL)
             {
                 perror("Error opening output file!\n");
                 return 0;
@@ -192,6 +184,7 @@ int main(int argc, int **argv)
         else /* PARENT PROCESS */
         {
             global_fileCounter++;
+            // printf("Counter: %d\n", global_fileCounter);
             *currentInputFileDir = '\0';
         }
     }
@@ -203,15 +196,17 @@ int main(int argc, int **argv)
     }
 
     /* READ BUFFER OUTPUTS */
-    char outputBuffer[OUTPUT_BUFFER_SIZE];
+    char *outputBuffer = malloc(OUTPUT_BUFFER_SIZE);
     int outputCharCount = 0;
+    int outputChar;
+
     for (int i = 0; i < filesCount; i++)
     {
         FILE *bufferFileStream;
+        // printf("Current output buffer: %s\n", bufferOutputDirs[i]);
         if ((bufferFileStream = fopen(bufferOutputDirs[i], "r")) != NULL)
         {
 
-            int outputChar;
             while (((outputChar = fgetc(bufferFileStream)) != EOF))
             {
                 outputBuffer[outputCharCount++] = (char)outputChar;
@@ -221,34 +216,30 @@ int main(int argc, int **argv)
         fclose(bufferFileStream);
     }
 
-    //printf("Content:\n%s", outputBuffer); /* DEBUG */
-
     /* WRITE TO THE OUTPUT FILE */
     FILE *outputFileStream;
 
-    char *outputDir = malloc(WORD_BUFFER);
+    char outputDir[WORD_BUFFER] = {0x0};
 
-    strcat(outputDir, ROOT_DIR);
-    strcat(outputDir, outputFileName);
-    // printf("Output dir: %s \n", outputDir); /* DEBUG */
+    sprintf(outputDir, "%s%s", ROOT_DIR, outputFileName);
+
     if ((outputFileStream = fopen(outputDir, "w")) == NULL)
     {
         perror("Error opening output.txt file: \n");
         exit(-1);
     }
 
-    fputs(outputBuffer, outputFileStream);
+    fprintf(outputFileStream, "%s", outputBuffer);
     fclose(outputFileStream);
 
     /* REMOVE BUFFER OUTPUTS */
-    for (int i = 0; i < filesCount; i++)
-    {
-        if(remove(bufferOutputDirs[i]) != 0)
-        {
-            printf("Unable to remove %s\n", bufferOutputDirs[i]);
-        }
-    }
-    
+    // for (int i = 0; i < filesCount; i++)
+    // {
+    //     if (remove(bufferOutputDirs[i]) != 0)
+    //     {
+    //         printf("Unable to remove %s\n", bufferOutputDirs[i]);
+    //     }
+    // }
 
     return 0;
 }
